0296a968cb0073abbb1b18fd415c75d348581cb3
Victor Ayala <vicmanayaari@gmail.com>
2025-06-24 00:35:06 +0000

    Here is the console error: The resource at “https://customizer-studio.fl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="data:;base64,iVBORw0KGgo=" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
