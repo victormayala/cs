d155f70a315f3f83ad007a490b35b7e0558027c9
Victor Ayala <vicmanayaari@gmail.com>
2025-08-28 23:10:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/next.config.ts
+++ src/next.config.ts
@@ -29,6 +29,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdnp.sanmar.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   async redirects() {
