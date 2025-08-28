0d3708dabca219bf1cc9489f57f29477335a86d8
Victor Ayala <vicmanayaari@gmail.com>
2025-08-28 19:50:56 +0000

    continue

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -4,12 +4,12 @@
 import Image from 'next/image';
 
 export function Logo() {
-  const logoSrc = "/customizer-studio-logo.png"; // Using the new filename
+  const logoSrc = "/customizer-studio-logo.png"; 
 
   return (
     <div className="relative h-12 w-[180px]" aria-label="Customizer Studio Logo">
       <Image
-        key={logoSrc} // Key helps if src might change, though with static new name it's less critical
+        key={logoSrc}
         src={logoSrc}
         alt="Customizer Studio Logo"
         fill
