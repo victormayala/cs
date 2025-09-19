46210f75b95e5f5020531416f2be640664a29e9d
Victor Ayala <vicmanayaari@gmail.com>
2025-09-19 19:58:43 +0000

    change it to 2.2

--- src/app/customizer/Customizer.tsx
+++ src/app/customizer/Customizer.tsx
@@ -633,7 +633,7 @@ export function Customizer() {
       const baseWidth = stageDimensions.width * box.width / 100;
 
       // New width (30% wider)
-      const calculatedWidth = baseWidth * 2.5;
+      const calculatedWidth = baseWidth * 2.2;
 
       // Shift X so it expands evenly left + right
       const extraWidth = calculatedWidth - baseWidth;
@@ -980,5 +980,3 @@ export function Customizer() {
     </div>
   );
 }
-
-    
